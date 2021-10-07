a77c62459e28f46b2bb2b775a372363221cd44ff
Agustin Garbarino <agusgarba@gmail.com>
2021-10-07 11:54:10 -0300

    Adding hotjar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { hotjar } from "react-hotjar";
 import Header from "./Header";
 
 function Layout(props) {
+  hotjar.initialize(2640397, 6);
   return (
     <div>
       <Header />
